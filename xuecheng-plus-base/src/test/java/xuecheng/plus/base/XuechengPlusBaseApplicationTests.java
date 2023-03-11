3af9d0b2cd81bd7b798b0cabcaafcfb3ec08c12c
liujinhao <1126183894@qq.com>
2023-03-11 09:32:26 +0800

    项目初始结构第一次提交

---
+++ xuecheng-plus-base/src/test/java/xuecheng/plus/base/XuechengPlusBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package xuecheng.plus.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
